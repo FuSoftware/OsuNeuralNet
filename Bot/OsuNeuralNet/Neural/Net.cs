8766be8fd935a1fb38952e28345f28f8774a1aa1
FlorentUguet <florent.uguet@gmail.com>
2016-12-17 14:50:53 +0100

    Imported C++ neural Network functionalities

---
+++ Bot/OsuNeuralNet/Neural/Net.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Layer = System.Collections.Generic.List<OsuNeuralNet.Neural.Neuron>;
+
+namespace OsuNeuralNet.Neural
+{
+    class Net
+    {
+        private List<Layer> m_layers; //m_layers[layerNum][neuronNum]
+        private double m_error;
+        private double m_recentAverageError;
+        private static double m_recentAverageSmoothingFactor;
+
+        public Net(List<int> topology)
+        {
+            int numLayers = topology.Count();
+            for (int layerNum = 0; layerNum < numLayers; ++layerNum)
+            {
+                m_layers.Add(new Layer());
+
+                //If the next layer isn't the last (output layer), uses the number of neurons in the next layer
+                int numOutputs = layerNum == topology.Count() - 1 ? 0 : topology[layerNum + 1];
+
+                //We have made a new Layer, now fill it with neurons, and
+                // add a bias neuron to the layer
+
+                for (int neuronNum = 0; neuronNum <= topology[layerNum]; ++neuronNum)
+                {
+                    m_layers.Last().Add(new Neuron(numOutputs, neuronNum));
+                }
+
+                //Forces the bias node's output value to 1.0. It's the last neuron created above
+                m_layers.Last().Last().setOutputVal(1.0);
+            }
+        }
+
+        public void feedForward(List<double> inputVals)
+        {
+
+            //Assign the inputs
+            for (int i = 0; i < inputVals.Count(); ++i)
+            {
+                m_layers[0][i].setOutputVal(inputVals[i]);
+            }
+
+            //Forward propagate
+            for (int layerNum = 1; layerNum < m_layers.Count(); ++layerNum)
+            {
+                Layer prevLayer = m_layers[layerNum - 1];
+                for (int n = 0; n < m_layers[layerNum].Count() - 1; ++n)
+                {
+                    m_layers[layerNum][n].feedForward(prevLayer);
+                }
+            }
+        }
+
+        public void backProp(List<double> targetVals)
+        {
+            // Calculate overall net error (RMS of output errors)
+            Layer outputLayer = m_layers.Last();
+            m_error = 0.0;
+
+            for (int n = 0; n < outputLayer.Count() - 1; ++n)
+            {
+                double delta = targetVals[n] - outputLayer[n].getOutputVal();
+                m_error += delta * delta;
+            }
+
+            m_error /= outputLayer.Count() - 1; //Get average error squared
+            m_error = Math.Sqrt(m_error); //RMS
+
+            //Implement a recent average measurement :
+            m_recentAverageError = (m_recentAverageError * m_recentAverageSmoothingFactor + m_error) / (m_recentAverageSmoothingFactor + 1.0);
+
+            // Calculate output layer gradients
+            for (int n = 0; n < outputLayer.Count() - 1; ++n)
+            {
+                outputLayer[n].calcOutputGradients(targetVals[n]);
+            }
+
+            //Calculate gradientso n hidden layers
+            for (int layerNum = m_layers.Count() - 2; layerNum > 0; --layerNum)
+            {
+                Layer hiddenLayer = m_layers[layerNum];
+                Layer nextLayer = m_layers[layerNum + 1];
+
+                for (int n = 0; n < hiddenLayer.Count(); ++n)
+                {
+                    hiddenLayer[n].calcHiddenGradients(nextLayer);
+                }
+            }
+
+            // For all layers from output to first hidden layer,
+            // update connection weights
+
+            for (int layerNum = m_layers.Count() - 1; layerNum > 0; --layerNum)
+            {
+                Layer layer = m_layers[layerNum];
+                Layer prevLayer = m_layers[layerNum - 1];
+
+                for (int n = 0; n < layer.Count() - 1; ++n)
+                {
+                    layer[n].updateInputWeights(prevLayer);
+                }
+            }
+        }
+
+        public void getResults(List<double> resultVals)
+        {
+            resultVals.Clear();
+
+            for (int n = 0; n < m_layers.Last().Count() - 1; ++n)
+            {
+                resultVals.Add(m_layers.Last()[n].getOutputVal());
+            }
+        }
+
+        public double getRecentAverageError()
+        {
+            return this.m_recentAverageError;
+        }
+    }
+}
---
+++ Bot/OsuNeuralNet/Neural/Neuron.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Layer = System.Collections.Generic.List<OsuNeuralNet.Neural.Neuron>;
+
+namespace OsuNeuralNet.Neural
+{
+    public class Connection
+    {
+        public double weight;
+        public double deltaWeight;
+    }
+
+    class Neuron
+    {
+        double m_outputVal;
+        List<Connection> m_outputWeights = new List<Connection>();
+        int m_myIndex;
+        double m_gradient;
+
+        static double alpha; //[0.0..1.0] overall net training rate
+        static double eta; //[0.0..n] multiplier of last weight charge (momentum)
+
+        public Neuron(int numOutputs, int myIndex)
+        {
+            for (int c = 0; c < numOutputs; ++c)
+            {
+                Connection conn = new Connection();
+                conn.weight = randomWeight();
+                m_outputWeights.Add(conn);
+            }
+
+            m_myIndex = myIndex;
+        }
+
+        public void setOutputVal(double val)
+        {
+            this.m_outputVal = val;
+        }
+
+        public double getOutputVal() 
+        {
+            return this.m_outputVal;
+        }
+
+        public void feedForward(Layer prevLayer)
+        {
+            double sum = 0;
+
+            //Sums the previous layer's outputs (our inputs)
+            //Include the bias node from the previous layer
+
+            for (int n = 0; n < prevLayer.Count(); ++n)
+            {
+                sum += prevLayer[n].getOutputVal() * prevLayer[n].m_outputWeights[m_myIndex].weight;
+            }
+
+            m_outputVal = transferFunction(sum);
+        }
+
+        public void calcOutputGradients(double targetVal)
+        {
+            double delta = targetVal - m_outputVal;
+            m_gradient = delta * transferFunctionDerivative(m_outputVal);
+        }
+
+        public void calcHiddenGradients(Layer nextLayer)
+        {
+            double dow = sumDOW(nextLayer);
+            m_gradient = dow * transferFunctionDerivative(m_outputVal);
+        }
+
+        public void updateInputWeights(Layer prevLayer)
+        {
+            //The weights to be updated are in the connection container
+            //in the neuron in the preceding layer
+
+            for (int n = 0; n < prevLayer.Count() - 1; ++n)
+            {
+                Neuron neuron = prevLayer[n];
+                double oldDeltaWeight = neuron.m_outputWeights[m_myIndex].deltaWeight;
+
+                double newDeltaWeight =
+                        //Individual input, magnified by the gradient and train rate
+                        eta
+                        * neuron.getOutputVal()
+                        * m_gradient
+                        //Also add momentum = a fraction of the previous delta weight
+                        + alpha
+                        * oldDeltaWeight;
+                neuron.m_outputWeights[m_myIndex].deltaWeight = newDeltaWeight;
+                neuron.m_outputWeights[m_myIndex].weight += newDeltaWeight;
+            }
+        }
+
+        private static double randomWeight()
+        {
+            Random r = new Random();
+            return r.NextDouble();
+        }
+        private static double transferFunction(double x)
+        {
+            //tanh - output range [-1.0..1.0]
+            return Math.Tanh(x);
+        }
+        private static double transferFunctionDerivative(double x)
+        {
+            //tanh derivative
+            return 1.0 - x * x;
+        }
+        private double sumDOW(Layer nextLayer)
+        {
+            double sum = 0.0;
+
+            //Sum our contributions of the errors at the nodes we feed
+
+            for (int n = 0; n < nextLayer.Count() - 1; ++n)
+            {
+                sum += m_outputWeights[n].weight * nextLayer[n].m_gradient;
+            }
+
+            return sum;
+        }
+    }
+}
